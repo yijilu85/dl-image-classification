afaf238c3c51fb6630f3e9a3c21f2e9ef5902612
yiji <yiji85@googlemail.com>
2026-04-25 19:46:00 +0200

    fix removing custom

--- src/components/ImageGroup.vue
+++ src/components/ImageGroup.vue
@@ -1,21 +1,23 @@
 <template>
-  elements: {{ visibleImages }}
+  elements: {{ visibleImages }} uplöoaded: {{ uploadedFilePreviews }}
   <div class="mt-2">
     <SingleImage
       v-for="(item, index) in visibleImages"
       :imgSrc="item"
       :correct="groupData.label"
       class="mt-4 mb-4 fade-item"
-      @remove="handleRemoveImage(index)"
+      @remove="handleRemoveImage(index, visibleImages)"
       :class="{ removing: removingIndex === index }"
     />
-    <div
-      v-for="preview in uploadedFilePreviews"
-      :key="preview.url"
-      class="preview-item"
-    >
-      <SingleImage :imgSrc="preview.url" :correct="true" />
-    </div>
+    <SingleImage
+      v-for="(item, uploadedIndex) in uploadedFilePreviews"
+      :key="item.url"
+      class="preview-item mt-4 mb-4 fade-item"
+      @remove="handleRemoveUploadedImage(uploadedIndex, uploadedFilePreviews)"
+      :class="{ removing: removingUploadedIndex === uploadedIndex }"
+      :imgSrc="item.url"
+      :correct="true"
+    />
     <FileUpload
       v-if="groupData.enableUpload"
       @file-selected="handleFilesSelected($event)"
@@ -35,20 +37,32 @@ const props = defineProps<{
 
 const visibleImages = ref<string[]>(props.groupData.images);
 const removingIndex = ref<number | null>(null);
+const removingUploadedIndex = ref<number | null>(null);
 
 const uploadedFilePreviews = ref<FilePreview[]>([]);
-// const files = ref<File[]>([]);
 const handleFilesSelected = (filePreview: FilePreview) => {
   console.log("File selected:", filePreview);
   uploadedFilePreviews.value.push(filePreview);
 };
 
-const handleRemoveImage = (index: number) => {
+const handleRemoveImage = (index: number, list: string[] | FilePreview[]) => {
   removingIndex.value = index;
+  console.log("Removing image at index:", index, list);
   setTimeout(() => {
-    visibleImages.value.splice(index, 1);
+    list.splice(index, 1);
     removingIndex.value = null;
-  }, 300); // match your transition duration
+  }, 300);
+};
+const handleRemoveUploadedImage = (
+  index: number,
+  list: string[] | FilePreview[],
+) => {
+  console.log("Removing image at index:", index, list);
+  removingUploadedIndex.value = index;
+  setTimeout(() => {
+    list.splice(index, 1);
+    removingUploadedIndex.value = null;
+  }, 300);
 };
 onMounted(async () => {
   console.log("Mounted ImageGroup with data:", props.groupData);
